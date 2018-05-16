6e6ef15d25214af759854b864aef2e1be9b3793c
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-05-16 14:23:54 +0100

    indexserver: Use a priority queue and API batching
    This is a major change to how indexserver works, and an increase in complexity.
    Our goal is to reduce indexing lag for large installations (10k+ repositories).
    In a large installation the majority of repositories will not need updating.
    However, the indexer was a simple for loop which checked the latest commit and
    then tried to index it incrementally. This meant we spent a large amount of time
    doing network requests (to find out the commit) and not any actual processing.
    
    The first change is to introduce a queue which contains (repo, commit). This
    allows us to have a dedicated goroutine which just pops jobs off and indexes
    them. This ensures we are always busy and not blocked by the Sourcegraph API.
    
    Additionally the queue is a priority queue. This means if we know a repo has a
    new commit it can be processed before a repo which has not had any changes. We
    still process queue we suspect haven't changed, just in case the underlying data
    has gone. This is also good since it means we don't couple how incremental
    updates work in zoekt-archive-index with our indexer.
    
    The second change is to concurrently check what the latest commit is for a
    repository. Gitserver can handle many concurrent resolve-rev requests, so we
    take advantage of that.

---
+++ cmd/zoekt-sourcegraph-indexserver/queue.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type queueItem struct {
+	// name is the name of the repo
+	name string
+	// indexed is the last known indexed commit
+	indexed string
+	// commit is the latest commit available from gitserver. It is the commit
+	// want to index next. It can be the same as current.
+	commit string
+	// heapIdx is the index of the item in the heap. If < 0 then the item is
+	// not on the heap.
+	heapIdx int
+	// seq is a sequence number used as a tie breaker. This is to ensure we
+	// act like a FIFO queue.
+	seq int64
+}
+
+// Queue is a priority queue which returns the next repo to index. It is safe
+// to use concurrently. It is a min queue on:
+//
+//    (indexed commit != latest commit, time added to the queue)
+//
+// We use the above since:
+//
+// * We rather index a repo sooner if we know the commit is stale.
+// * The order of repos returned by Sourcegraph API are ordered by importance.
+type Queue struct {
+	mu    sync.Mutex
+	items map[string]*queueItem
+	pq    pqueue
+	seq   int64
+}
+
+// Pop returns the name and commit of the next repo to index. If the queue is
+// empty ok is false.
+func (q *Queue) Pop() (name string, commit string, ok bool) {
+	q.mu.Lock()
+	if len(q.pq) == 0 {
+		q.mu.Unlock()
+		return "", "", false
+	}
+	item := heap.Pop(&q.pq).(*queueItem)
+	name = item.name
+	commit = item.commit
+	q.mu.Unlock()
+	return name, commit, true
+}
+
+// Len returns the number of items in the queue.
+func (q *Queue) Len() int {
+	q.mu.Lock()
+	l := len(q.pq)
+	q.mu.Unlock()
+	return l
+}
+
+// AddOrUpdate sets which commit to index next for name. If name is already in
+// the queue, it is updated.
+func (q *Queue) AddOrUpdate(name, commit string) {
+	q.mu.Lock()
+	item := q.get(name)
+	item.commit = commit
+	if item.heapIdx < 0 {
+		q.seq++
+		item.seq = q.seq
+		heap.Push(&q.pq, item)
+	} else {
+		heap.Fix(&q.pq, item.heapIdx)
+	}
+	q.mu.Unlock()
+}
+
+// SetIndexed sets what the currently indexed commit is for name.
+func (q *Queue) SetIndexed(name, indexed string) {
+	q.mu.Lock()
+	item := q.get(name)
+	item.indexed = indexed
+	if item.heapIdx >= 0 {
+		// We only update the position in the queue, never add it.
+		heap.Fix(&q.pq, item.heapIdx)
+	}
+	q.mu.Unlock()
+}
+
+// get returns the item for name. If the name hasn't been seen before, it is
+// added to q.items.
+//
+// Note: get requires that q.mu is held.
+func (q *Queue) get(name string) *queueItem {
+	if q.items == nil {
+		q.items = map[string]*queueItem{}
+		q.pq = make(pqueue, 0)
+	}
+
+	item, ok := q.items[name]
+	if !ok {
+		item = &queueItem{
+			name:    name,
+			heapIdx: -1,
+		}
+		q.items[name] = item
+	}
+
+	return item
+}
+
+// pqueue implements a priority queue via the interface for container/heap
+type pqueue []*queueItem
+
+func (pq pqueue) Len() int { return len(pq) }
+
+func (pq pqueue) Less(i, j int) bool {
+	// If we know a needs an update and b doesn't, then return true. Otherwise
+	// they are either equal priority or b is more urgent.
+	a := pq[i]
+	b := pq[j]
+	if (a.indexed == a.commit) == (b.indexed == b.commit) {
+		// tie breaker is to prefer the item added to the queue first
+		return a.seq < b.seq
+	}
+	return a.indexed != a.commit
+}
+
+func (pq pqueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].heapIdx = i
+	pq[j].heapIdx = j
+}
+
+func (pq *pqueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*queueItem)
+	item.heapIdx = n
+	*pq = append(*pq, item)
+}
+
+func (pq *pqueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.heapIdx = -1
+	*pq = old[0 : n-1]
+	return item
+}
